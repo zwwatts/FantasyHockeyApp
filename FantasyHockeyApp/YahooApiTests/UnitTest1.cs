cd7390ac1aab21f0991cbe259fd2ab40f23f783d
Noah Brusky <bruskynd@msoe.edu>
2017-01-15 11:41:07 -0600

    added the yahoo api query capability

---
+++ FantasyHockeyApp/YahooApiTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YahooApi;
+
+namespace YahooApiTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var yahoo = new Yahoo();
+            Debug.WriteLine(yahoo.GetLeague(22381));
+        }
+    }
+}
