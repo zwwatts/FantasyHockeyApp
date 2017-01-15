dde5bafb8dbe035f5d4d75affb4cd7361bcf70ba
Noah Brusky <bruskynd@msoe.edu>
2017-01-15 11:22:18 -0600

    oauth is working, can query a league

---
+++ FantasyHockeyApp/YahooApi/Yahoo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahooApi
+{
+    public class Yahoo
+    {
+        public Yahoo()
+        {
+            
+        }
+
+        public string GetLeague(int leagueId)
+        {
+            var oauthQuery = new OAuthQuery();
+            return oauthQuery.QueryWithOAuth(@"http://fantasysports.yahooapis.com/fantasy/v2/league/363.l.22381");
+        }
+    }
+}
