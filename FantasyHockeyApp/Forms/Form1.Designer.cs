2cfd3435d682c215ac2436fe8ba741be0cbcf711
Nate Dobbins <dobbinsn@msoe.edu>
2017-01-15 11:44:46 -0600

    added testing project, and continued UI work

--- FantasyHockeyApp/Forms/Form1.Designer.cs
+++ FantasyHockeyApp/Forms/Form1.Designer.cs
@@ -35,14 +35,20 @@
             this.Matchups = new System.Windows.Forms.TabPage();
             this.matchupGridView = new System.Windows.Forms.DataGridView();
             this.Teams = new System.Windows.Forms.TabPage();
-            this.teamGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.teamComboBox = new System.Windows.Forms.ComboBox();
+            this.teamPlayerGridView = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
             this.Standings.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.standingsGridView)).BeginInit();
             this.Matchups.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.matchupGridView)).BeginInit();
             this.Teams.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.teamGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamPlayerGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // appName
@@ -50,8 +56,9 @@
             this.appName.Dock = System.Windows.Forms.DockStyle.Top;
             this.appName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.appName.Location = new System.Drawing.Point(0, 0);
+            this.appName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.appName.Name = "appName";
-            this.appName.Size = new System.Drawing.Size(802, 22);
+            this.appName.Size = new System.Drawing.Size(791, 27);
             this.appName.TabIndex = 0;
             this.appName.Text = "Yahoo Fantasy Hockey App";
             this.appName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -62,19 +69,21 @@
             this.tabControl1.Controls.Add(this.Matchups);
             this.tabControl1.Controls.Add(this.Teams);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 22);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(802, 481);
+            this.tabControl1.Size = new System.Drawing.Size(791, 418);
             this.tabControl1.TabIndex = 1;
             // 
             // Standings
             // 
             this.Standings.Controls.Add(this.standingsGridView);
-            this.Standings.Location = new System.Drawing.Point(4, 22);
+            this.Standings.Location = new System.Drawing.Point(4, 25);
+            this.Standings.Margin = new System.Windows.Forms.Padding(4);
             this.Standings.Name = "Standings";
-            this.Standings.Padding = new System.Windows.Forms.Padding(3);
-            this.Standings.Size = new System.Drawing.Size(794, 455);
+            this.Standings.Padding = new System.Windows.Forms.Padding(4);
+            this.Standings.Size = new System.Drawing.Size(1020, 437);
             this.Standings.TabIndex = 0;
             this.Standings.Text = "Standings";
             this.Standings.UseVisualStyleBackColor = true;
@@ -83,19 +92,21 @@
             // 
             this.standingsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.standingsGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.standingsGridView.Location = new System.Drawing.Point(3, 3);
+            this.standingsGridView.Location = new System.Drawing.Point(4, 4);
+            this.standingsGridView.Margin = new System.Windows.Forms.Padding(4);
             this.standingsGridView.Name = "standingsGridView";
-            this.standingsGridView.Size = new System.Drawing.Size(788, 449);
+            this.standingsGridView.ReadOnly = true;
+            this.standingsGridView.Size = new System.Drawing.Size(1012, 429);
             this.standingsGridView.TabIndex = 0;
-            this.standingsGridView.ColumnCount = 7;
             // 
             // Matchups
             // 
             this.Matchups.Controls.Add(this.matchupGridView);
-            this.Matchups.Location = new System.Drawing.Point(4, 22);
+            this.Matchups.Location = new System.Drawing.Point(4, 25);
+            this.Matchups.Margin = new System.Windows.Forms.Padding(4);
             this.Matchups.Name = "Matchups";
-            this.Matchups.Padding = new System.Windows.Forms.Padding(3);
-            this.Matchups.Size = new System.Drawing.Size(794, 455);
+            this.Matchups.Padding = new System.Windows.Forms.Padding(4);
+            this.Matchups.Size = new System.Drawing.Size(1020, 437);
             this.Matchups.TabIndex = 1;
             this.Matchups.Text = "Matchups";
             this.Matchups.UseVisualStyleBackColor = true;
@@ -104,38 +115,103 @@
             // 
             this.matchupGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.matchupGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.matchupGridView.Location = new System.Drawing.Point(3, 3);
+            this.matchupGridView.Location = new System.Drawing.Point(4, 4);
+            this.matchupGridView.Margin = new System.Windows.Forms.Padding(4);
             this.matchupGridView.Name = "matchupGridView";
-            this.matchupGridView.Size = new System.Drawing.Size(788, 449);
+            this.matchupGridView.ReadOnly = true;
+            this.matchupGridView.Size = new System.Drawing.Size(1012, 429);
             this.matchupGridView.TabIndex = 0;
             // 
             // Teams
             // 
-            this.Teams.Controls.Add(this.teamGridView);
-            this.Teams.Location = new System.Drawing.Point(4, 22);
+            this.Teams.Controls.Add(this.dataGridView1);
+            this.Teams.Controls.Add(this.label3);
+            this.Teams.Controls.Add(this.label2);
+            this.Teams.Controls.Add(this.label1);
+            this.Teams.Controls.Add(this.teamComboBox);
+            this.Teams.Controls.Add(this.teamPlayerGridView);
+            this.Teams.Location = new System.Drawing.Point(4, 25);
+            this.Teams.Margin = new System.Windows.Forms.Padding(4);
             this.Teams.Name = "Teams";
-            this.Teams.Padding = new System.Windows.Forms.Padding(3);
-            this.Teams.Size = new System.Drawing.Size(794, 455);
+            this.Teams.Padding = new System.Windows.Forms.Padding(4);
+            this.Teams.Size = new System.Drawing.Size(783, 389);
             this.Teams.TabIndex = 2;
             this.Teams.Text = "Teams";
             this.Teams.UseVisualStyleBackColor = true;
             // 
-            // teamGridView
+            // dataGridView1
             // 
-            this.teamGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.teamGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.teamGridView.Location = new System.Drawing.Point(0, 0);
-            this.teamGridView.Name = "teamGridView";
-            this.teamGridView.Size = new System.Drawing.Size(794, 455);
-            this.teamGridView.TabIndex = 0;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(777, 190);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Goalies";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Skaters";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(655, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Your Team:";
+            // 
+            // teamComboBox
+            // 
+            this.teamComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.teamComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.teamComboBox.FormattingEnabled = true;
+            this.teamComboBox.Location = new System.Drawing.Point(655, 38);
+            this.teamComboBox.Name = "teamComboBox";
+            this.teamComboBox.Size = new System.Drawing.Size(121, 24);
+            this.teamComboBox.TabIndex = 1;
+            this.teamComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // teamPlayerGridView
+            // 
+            this.teamPlayerGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.teamPlayerGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.teamPlayerGridView.Location = new System.Drawing.Point(3, 78);
+            this.teamPlayerGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.teamPlayerGridView.Name = "teamPlayerGridView";
+            this.teamPlayerGridView.ReadOnly = true;
+            this.teamPlayerGridView.Size = new System.Drawing.Size(777, 94);
+            this.teamPlayerGridView.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 503);
+            this.ClientSize = new System.Drawing.Size(791, 445);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.appName);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
@@ -144,7 +220,9 @@
             this.Matchups.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.matchupGridView)).EndInit();
             this.Teams.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.teamGridView)).EndInit();
+            this.Teams.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamPlayerGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -158,7 +236,12 @@
         private System.Windows.Forms.TabPage Teams;
         private System.Windows.Forms.DataGridView standingsGridView;
         private System.Windows.Forms.DataGridView matchupGridView;
-        private System.Windows.Forms.DataGridView teamGridView;
+        private System.Windows.Forms.DataGridView teamPlayerGridView;
+        private System.Windows.Forms.ComboBox teamComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
